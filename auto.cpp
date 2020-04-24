0186455e563c805169008b5a2e1914b03d67476e
eugenie8 <yoojin7856@gmail.com>
2020-04-24 21:31:40 +0900

    Auto
    practiced auto in cpp

---
+++ auto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+
+    cin >> a;
+    for (auto c : a)
+    {
+        cout << a << endl;
+    }
+    return 0;
+}
