49022e6373ebece71a29f4e12a693217eace9922
eugenie8 <yoojin7856@gmail.com>
2020-04-27 19:00:52 +0900

    Initialize string
    Practiced initializing string ​​in cpp

---
+++ initial_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    a = string(10, 'X');
+    cout << a << endl;
+    a = string(3, '3');
+    cout << a << endl;
+
+    return 0;
+}
