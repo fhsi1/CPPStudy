92f94ef18ed604cbda54e60fecdf265577ce4538
eugenie8 <yoojin7856@gmail.com>
2020-04-28 15:20:53 +0900

    Set erase element
    Practiced erasing element in set

---
+++ set_erase.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    string a;
+
+    cin >> a;
+    set<char> b(a.begin(), a.end());
+    for (auto bb : b)
+        cout << bb << " ";
+    cout << endl;
+
+    char c;
+
+    cin >> c;
+    if (b.find(c) != b.end())
+        b.erase(c);
+    for (auto bb : b)
+        cout << bb << " ";
+    cout << endl;
+
+    return 0;
+}
