5a9c7127b51df06d48c261681bedd07211785da6
eugenie8 <yoojin7856@gmail.com>
2020-04-30 21:34:41 +0900

    min() and max()
    Practiced min()and max() ​​in cpp

---
+++ min_max.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << "max : " << max(a, b) << endl;
+    cout << "min : " << min(a, b) << endl;
+
+    return 0;
+}
